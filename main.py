f0511b2d9793d78f149dbeccecc6135f01992ab2
Apex <ilind663@gmail.com>
2025-02-04 15:41:45 +0300

    last finall

--- main.py
+++ main.py
@@ -856,16 +856,6 @@ class Heavy_Armor(Armor):
         self.hp = 300
 
 
-# class Wall(pygame.sprite.Sprite):
-#     def __init__(self, x, y, width, height):
-#         super().__init__(all_sprites, walls)
-#         self.image = pygame.Surface((width, height))
-#         self.image.fill(pygame.Color("black"))
-#         self.rect = self.image.get_rect()
-#         self.rect.x = x
-#         self.rect.y = y
-
-
 class Particle(pygame.sprite.Sprite):
     def __init__(self, pos, dx, dy, surface):
         super().__init__(all_sprites)
